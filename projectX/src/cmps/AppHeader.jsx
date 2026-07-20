f4c2cf719c36dd84f21edd1b36887053a7060141
AndreiYakerson <tubikwheels@gmail.com>
2026-07-20 22:13:20 +0300

    Add basic app header

---
+++ projectX/src/cmps/AppHeader.jsx
@@ -0,0 +1,23 @@
+
+
+
+export function AppHeader() {
+
+    return (
+        <header className="app-header">
+            <section className="app-logo">
+                <h1>Focus</h1>
+            </section>
+            <nav className="app-nav">
+                <ul>
+                    <li><a href="/">Обьявления</a></li>
+                    <li><a href="/about">Категории</a></li>
+                    <li><a href="/contact">Поддержка</a></li>
+                </ul>
+            </nav>
+
+
+        </header>
+
+    )
+}
